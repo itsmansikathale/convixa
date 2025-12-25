47998bcd2e9529016684db69897be4dbc6b36329
Manasi V Kathale <mansikathale5@gmail.com>
2025-12-25 22:05:34 +0530

    Added logo & para on main page

--- app/page.js
+++ app/page.js
@@ -14,15 +14,24 @@ export default function Home() {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-linear-to-br from-gray-900 via-gray-800 to-gray-900">
-      <div className="p-8 bg-gray-700/80 rounded-2xl border border-gray-600 w-70 shadow-2xl">
-        <h2 className="text-md mb-4 font-normal text-center ">
+    <div className="flex justify-center items-center min-h-screen bg-linear-to-br from-blue-900 via-gray-800 to-purple-900">
+      <div className="p-4 bg-#928484-700/80 rounded-2xl border border-gray-600 w-104 h-120 shadow-2xl">
+        <p className="text-sm mt-1 text-center">
+          Connect, collaborate, and let AI handle the rest.
+        </p>
+        <img
+          src="c_logo.png"
+          alt="Convixa logo"
+          className="h-60 w-auto pb-4  ml-20 items-center  justify-center"
+        />
+
+        <h2 className="text-sm mb-4 text-gray-200 font-normal text-center ">
           Enter Your Name
         </h2>
 
         <input
-          placeholder="e.g Manasi"
-          className="px-4 py-3 rounded-lg w-full bg-gray-800/70 border border-gray-900/60 text-white"
+          placeholder="e.g Mansi"
+          className="px-4 py-3 rounded-lg w-full bg-#5e4949-800/70 border border-gray-900/60 text-gray-200"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
